b51ae09b03a8bae6a8f5e4f55fcef9d932e74db3
zalohome <56580150+zalohome@users.noreply.github.com>
2023-07-29 21:22:01 +0700

    Update connect_db.php

--- includes/connect_db.php
+++ includes/connect_db.php
@@ -1,1 +1,11 @@
 
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "demo_sql";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+?>
