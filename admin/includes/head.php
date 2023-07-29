290290f741e0856ee5898fa182cf75bb8edec37f
zalohome <56580150+zalohome@users.noreply.github.com>
2023-07-29 21:24:00 +0700

    Add files via upload

---
+++ admin/includes/head.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Admin</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+  
+  <link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css"
+/>
+
+<script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+
+
+</head>
+
+<body>
+  <div class="container-fluid">
+
+    <nav class="container ">
+      <div class="header d-flex justify-content-between container " style="height: 4rem;">
+        <a  class="navbar-brand text-dark my-auto " href="" id="text-logo">Start Bootstrap</a>
+        <ul>
+          <li><a href="/admin/feature.php">Feature</a></li>
+        </ul>
+        <div class="my-auto "><button class="btn bg-primary" id="button-sign-up">Sign Up</button></div>
+  
+      </div>
+    </nav>
